6cc1b5bf2aef015186772ec538b91a8ebdf9562c
Jordan Montgomery <jmontgom.10@gmail.com>
2017-01-12 22:13:37 -0500

    Added tests for combine_images utility
    Error propagation in the combine_images utility can now rightly be
    trusted (as much as any code really can be trusted).

--- tests/utils_tests.py
+++ tests/utils_tests.py
@@ -28,24 +28,21 @@ def test_combine_images_sum():
             'AIRMASS':1 + 0.1*i})
         imgList.append(tmpImg)
 
-    # Generate a null image
-    nullImg = imgList[0].copy()
-    nullImg.arr = 0*imgList[0].arr
+    # Make a copy of the first image in the imgList
+    resImg1 = imgList[0].copy()
 
-    # Make a copy of the null image to add up the image list
-    sumImg1 = nullImg.copy()
-
-    # Manually compute the summed image
-    for img in imgList:
-        sumImg1 += img
+    # Manually compute the summed image from the remaining images
+    for img in imgList[1:]:
+        resImg1 += img
 
     # Compute the sum of the imgList using the utilities
-    sumImg2 = utils.combine_images(imgList, output='sum')
+    resImg2 = utils.combine_images(imgList, output='sum')
 
     # Assert that the numpy and AstroImage yield the same result
-    assert (sumImg1.arr == sumImg2.arr).all()
+    assert (resImg1.arr == resImg2.arr).all()
+    assert (resImg1.arr == 26).all()
+    assert (resImg1.sigma == resImg2.sigma).all()
 
-# Start by testing the most algebraically intensive function...
 def test_combine_images_mean():
     # Set test image shape
     ny, nx = (100, 100)
@@ -63,22 +60,72 @@ def test_combine_images_mean():
             'AIRMASS':1 + 0.1*i})
         imgList.append(tmpImg)
 
-    # Generate a null image
-    nullImg = imgList[0].copy()
-    nullImg.arr = 0*imgList[0].arr
+    # Make a copy of the first image in the imgList
+    resImg1 = imgList[0].copy()
 
-    # Make a copy of the null image to add up the image list
-    sumImg1 = nullImg.copy()
+    # Manually compute the summed image from the remaining images
+    for img in imgList[1:]:
+        resImg1 += img
 
-    # Manually compute mean
-    for img in imgList:
-        sumImg1 += img
-
-    sumImg1 /= len(imgList)
+    # Convert the summed image to a mean by dividing by the number of
+    # images in the imgList
+    resImg1 /= len(imgList)
 
     # Compute the mean of the imgList using the utilities
-    sumImg2 = utils.combine_images(imgList, output='mean')
+    resImg2 = utils.combine_images(imgList, output='mean')
 
     # Assert that the numpy and AstroImage yield the same result
-    assert (sumImg1.arr == sumImg2.arr).all()
-    # assert_equal(sumImg)
+    assert (resImg1.arr == resImg2.arr).all()
+    assert (resImg1.arr == 6.5).all()
+    assert (resImg1.sigma == resImg2.sigma).all()
+
+def test_combine_images_weighted_mean():
+    # Set test image shape
+    ny, nx = (100, 100)
+
+    imgList = []
+    arrList  = []
+    sigList  = []
+    for i in range(4):
+        tmpImg = AstroImage()
+        tmpImg.arr     = np.zeros((ny, nx)) + 5 + i
+        tmpImg.sigma   = np.zeros((ny, nx)) + 2 + i
+        tmpImg.dtype   = tmpImg.arr.dtype
+        tmpImg.binning = (1, 1)
+        tmpImg.header  = fits.Header(cards={
+            'NAXIS1':nx,
+            'NAXIS2':ny,
+            'AIRMASS':1 + 0.1*i})
+        imgList.append(tmpImg)
+
+        # Build array list
+        arr = tmpImg.arr.copy()
+        arrList.append(arr)
+
+        # Build uncertainty list
+        sigma = tmpImg.sigma.copy()
+        sigList.append(sigma)
+
+    # Manually compute the summed image from the remaining images
+    resArr = 0*imgList[0].arr
+    resSig = 0*imgList[0].sigma
+    for arr, sig in zip(arrList, sigList):
+        resArr += (arr/(sig**2))
+
+    for sig in sigList:
+        resSig += 1.0/(sig**2)
+
+    # Normalize the result by the sum of the weights
+    resArr /= resSig
+
+    # Compute the uncertainty in the weighted mean
+    sig_resArr = 1.0/np.sqrt(resSig)
+
+    # Compute the mean of the imgList using the utilities
+    resImg2 = utils.combine_images(imgList, output='weighted_mean')
+
+    # Assert that the numpy and AstroImage yield the same result
+    assert (resArr == resImg2.arr).all()
+    assert (np.abs(resImg2.arr - 5.768124626) < 1e-5).all()
+    assert (sig_resArr == resImg2.sigma).all()
+    assert (np.abs(resImg2.sigma - 1.468666139) < 1e-5).all()
