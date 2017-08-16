6bf407a1807c1d42793f46a40472974d70347a24
Jordan Montgomery <jmontgom.10@gmail.com>
2017-08-15 23:57:08 -0400

    Major repairs for the adaptive mesher functionalility.

--- astroimage/utilitywrappers/adaptivemesher.py
+++ astroimage/utilitywrappers/adaptivemesher.py
@@ -29,6 +29,24 @@ class AdaptiveMesher(object):
         def binningStatistic(img):
             return img.data/img.uncertainty
 
+    upbinFunction: `function`
+        A user defined funciton which returns the binned input object to the
+        original binning scale. This function should be used to guarantee that
+        all the data are correctly rebinned rather than simply assuming that the
+        inverse `__getitem__` procedure will produce the correct results.
+
+        Example format:
+
+        def upbinData(inputImg, rebinFactor):
+            # Compute the output image shape
+            ny, nx = [rebinFactor*d for d in inputImg.shape]
+            outputImg = inputImg.rebin((ny, nx))
+
+            # Perhaps I need to adjust the rebinned uncertainties
+            outputImg.uncertainty = outputImg.uncertainty/np.sqrt(rebinFactor**2)
+
+            return outputImg
+
     dataCopier: `function`
         A user defined funciton which takes two input objects (e.g., image or
         StokesParameter object) and a set of indices and copies data from one
@@ -94,7 +112,7 @@ class AdaptiveMesher(object):
     >>> PA.show(vmin=0, vmax=180, cmap='rainbow')
     """
 
-    def __init__(self, inputObject, binningStatistic, dataCopier):
+    def __init__(self, inputObject, binningStatistic, upbinFunction, dataCopier):
         """Builds the AdaptiveMesher object."""
         # Test if the input object has a __getitem__ method defined
         if not hasattr(inputObject, '__getitem__'):
@@ -110,6 +128,9 @@ class AdaptiveMesher(object):
         # Store the binningStatistic function
         self.__binningStatistic = binningStatistic
 
+        # Store the upbinning function
+        self.__upbinFunction = upbinFunction
+
         # Store the cdataCpier function
         self.__dataCopier = dataCopier
 
@@ -118,12 +139,12 @@ class AdaptiveMesher(object):
         """Provides a quick reference to the original input"""
         return self._AdaptiveMesher__inputObject
 
-    def _prepare_data(self, maxRebins):
+    def _crop_data(self):
         """Crops data so rebinning produces an integer number of cells"""
         # Begin by performing all the necessary rebinnings and storing the
         # results in a dictionary. The first step of this is to pad or crop the
         # image accordingly.
-        maxBinning = 2**(maxRebins)
+        maxBinning = np.max(self.rebinLevels)
         ny, nx = self.data.shape
         cutRt  = nx % maxBinning
         cutTp  = ny % maxBinning
@@ -141,13 +162,12 @@ class AdaptiveMesher(object):
         # Store the cropped data
         self.croppedData = tmpData
 
-    def _compute_rebinnings(self, maxRebins):
+    def _compute_rebinnings(self):
         """Computes all possible rebinnings of the input data"""
         # Having cropped the Image (or StokesParameters), perform all possible
         # rebinnings and store them in a dictionary
         rebinDict  = {}
-        rebinnings = 2**np.arange(maxRebins + 1)
-        for rebin in rebinnings:
+        for rebin in self.rebinLevels:
             # Down-size the array via rebinning
             tmpData = self.croppedData[::rebin, ::rebin]
 
@@ -157,116 +177,143 @@ class AdaptiveMesher(object):
         # Store the rebinned data
         self.rebinnedData = rebinDict
 
-    def _test_refine_binning(self, finerBinningBool):
+    def _test_rebinnings(self, thresholdValue):
         """
-        Returns an array indicating which pixels can use a finer rebinning
+        Tests the SNR of each rebin level
 
-        To pass this test, clusters of 2x2 pixels must have at least 3 pixels
-        pass the test statistic at the *finer* binning.
+        To pass this test, clusters of 2x2 pixels must have at least all
+        four sub-pixels pass the test statistic at the *finer* binning.
 
         Parameters
         ----------
-        finerBinningBool: numpy.array
-            Array indicating which individual pixels passed the threshold test
-            at the finer binning level.
+        thresholdValue : `int` or `float`
+            The minimum value for the binningStatistic (defined by the function)
+            provided when initalizing the objet). Any cells which do not meet
+            this thresholding value will continue to be rebinned until the
+            binningStatistic function returns a value greater than this minimum
+            thresholding value.
         """
-        # Convert the arrays to ReducedScience images for rebinning
-        fineBoolImg = ReducedScience(finerBinningBool.astype(float))
-
-        # Grab the shape of the current finerBinningBool array
-        ny, nx = fineBoolImg.shape
-
-        # Rebin the fineTest image to see which 2x2 clusters have at least 3
-        # pixels passing the threshold value test at the finer binning level.
-        binnedTest       = fineBoolImg.rebin((ny//2, nx//2))
-        binnedTest       = 4*binnedTest.rebin((ny, nx))
-        finerBinningPass = binnedTest.data >= 3
-
-        return finerBinningPass
-
-    def _parse_rebinnings(self, thresholdValue):
-        """Determines the minimum rebinning which meets thresholdValue."""
-        # Grab the available rebinning levels
-        rebinnings      = np.array([k for k in self.rebinnedData.keys()])
-        sortInds        = rebinnings.argsort()
-        reverseSortInds = sortInds[::-1]
-        forwardBinnings = rebinnings[sortInds]
-        reverseBinnings = rebinnings[reverseSortInds]
-
-        # Loop through each rebinning (from greatest to least) and test if
-        # the supplied statistic meets the selected threshold value
-        testResults = {}
-        for rebin in reverseBinnings:
-            # If the statistic meets the threshold, then store that rebinning
-            testStat           = self._AdaptiveMesher__binningStatistic(self.rebinnedData[rebin])
-            testResults[rebin] = (testStat >= thresholdValue).astype(int)
-
-        # Grab the original shape of the unbinned cropped data
+        # Grab the shape of the *unbinned* data
         unbinnedShape = self.croppedData.shape
 
-        # Start by assuming the maximal binning level, and only permit
-        # refinement if 3-out-of-4 sub-pixels pass the threshold at the finer
-        # binning level
-        finalBinnings = np.ones(unbinnedShape, dtype=int)
+        # Loop through each rebin level and test if all four of its
+        # sub-pixels pass the SNR test.
+        binningTestDict = {}
+        for rebin in self.rebinLevels[:-1]:
+            # Compute the supplied test statistic
+            testStatistic = self._AdaptiveMesher__binningStatistic(self.rebinnedData[rebin])
 
-        # Loop back through the rebinnings and determine the ideal binning
-        for rebin in reverseBinnings[1:]:
-            # Test whether the next finer rebinning level passes the 3-pixel test
-            finerBinningPass = self._test_refine_binning(testResults[rebin])
+            # Rebin the fineTest image to see which 2x2 clusters have all four
+            # pixels passing the threshold value test at the finer binning level.
+            # Convert the arrays to ReducedScience images for rebinning
+            testStatisticImg = ReducedScience(
+                (testStatistic  > thresholdValue).astype(float)
+            )
 
-            # Now that it has been determined if this finer binning passes the
-            # test, expand the test results up to the original unbined shape
-            finerBinningPass = ReducedScience(finerBinningPass.astype(int))
-            finerBinningPass = finerBinningPass.rebin(unbinnedShape)
+            # Grab the shape of the current array
+            ny, nx = testStatistic.shape
 
-            # Store the results of this rebinning test
-            # First, figure out which parts of the image *can* have their
-            # binning level updated. Any pixels which have already had a binning
-            # level assignd *cannot* have their binning level updated. If the
-            # propesd finer binning level *failed* the finerBinningPass test,
-            # then assign it the *previous* level of binning (i.e., rebin//2)
+            # Check if all four pixels in each cluster pass the binning test
+            binnedTest  = testStatisticImg.rebin((ny//2, nx//2))
+            binnedTest  = binnedTest.rebin((ny, nx))
+            binningPass = (binnedTest.data == 1).astype(int)
 
-            # Locate the pixels which passed this proposed binning level
-            failedPixels = (finerBinningPass.data == 0)
+            # Expand the binnig test using the ReducedScience `rebin` method
+            binningPass = ReducedScience(binningPass).rebin(unbinnedShape)
 
-            # Locate any pixels which have already had a binning level assined
-            unassignedPixels = (finalBinnings == 1)
+            # Store the results in the binningTestDict dictionary
+            binningTestDict[rebin] = binningPass.data
+        else:
+            # Treat the final rebin level
+            rebin = self.rebinLevels[-1]
 
-            # The pixels to be assigned at this pass are those which failed and
-            # are currently 'unassigned' to a specific binning level
-            assignPixels = np.logical_and(failedPixels, unassignedPixels)
+            # Compute the supplied test statistic
+            testStatistic = self._AdaptiveMesher__binningStatistic(self.rebinnedData[rebin])
 
-            # Assign the identified pixels to the *previous* binning level
-            finalBinnings[np.where(assignPixels)] = rebin*2
+            # Determine if this statistic passed the test
+            binningPass = (testStatistic  > thresholdValue).astype(int)
+            binningPass = ReducedScience(binningPass).rebin(unbinnedShape)
 
-        # Locate pixels which do not ever pass the *maximal* binning
-        maximalBinningPass = self._test_refine_binning(testResults[forwardBinnings.max()])
-        maximalBinningPass = ReducedScience(maximalBinningPass.astype(int))
-        maximalBinningPass = maximalBinningPass.rebin(unbinnedShape)
+            # Store the results of this final binning level
+            binningTestDict[rebin] = binningPass.data
 
-        # Find pixels assigned to the maximal binning but which do not pass the
-        # maximal binning test
-        totalFailurePix = np.logical_and(
-            finalBinnings == forwardBinnings.max(),
-            np.logical_not(maximalBinningPass.data)
-        )
+        # Store the binning tests in the object for later reference
+        self.binningTestResults = binningTestDict
 
-        # Assign those pixels which fail even at the maximal binning to actually
-        # have the finest binning (since no value was obtained with binning up
-        # pixels)
-        finalBinnings[np.where(totalFailurePix)] = 1
+    def _assign_final_binnings(self):
+        """"""
+        # Generate an array in which to store the final binning levels
+        self.amrBinnings = -1*np.ones(self.croppedData.shape)
 
-        # Store the final binings for the user to check out if they want
-        self.amrBinnings = finalBinnings
+        # Create a *reverse* order array of rebin levels
+        reverseSortInds    = self.rebinLevels.argsort()[::-1]
+        reverseRebinLevels = self.rebinLevels[reverseSortInds]
 
+        # Loop through each binning level (except the 1x1 binning) and test if
+        # the binning passed the minimum threshold value
+        for rebin in reverseRebinLevels[:-1]:
+            # Determine if this binning level passes the test
+            thisRebinTest = self.binningTestResults[rebin]
+
+            # Determine if the next level down passes the test
+            nextRebinTest = self.binningTestResults[rebin//2]
+
+            # If this level passed the test and the next level down passed the
+            # test, then keep digging. However, if this level passed the test,
+            # but the next level down did not pass the test, then use this level
+            proposedPixelsToAssign = np.logical_and(
+                thisRebinTest,
+                np.logical_not(nextRebinTest)
+            )
+
+            # Locate which pixels have *already* been assigned to a binning
+            unassignedPixels = self.amrBinnings < 0
+
+            # Locate which pixels have not yet been assigned but should be
+            assignPixels = np.logical_and(
+                proposedPixelsToAssign,
+                unassignedPixels
+            )
+            assignInds = np.where(assignPixels)
+
+            # Assign these pixels to the current rebin level
+            self.amrBinnings[assignInds] = rebin
+        else:
+            # Determine which pixels to permit with the *finest* binning level
+            rebin = reverseRebinLevels[-1]
+
+            # Determine if this binning level passes the test
+            thisRebinTest = self.binningTestResults[rebin]
+
+            # Locate which pixels have *already* been assigned to a binning
+            unassignedPixels = self.amrBinnings < 0
+
+            # Locate which pixels have not yet been assigned but should be
+            assignPixels = np.logical_and(
+                thisRebinTest,
+                unassignedPixels
+            )
+            assignInds = np.where(assignPixels)
+
+            # Assign these pixels to the current rebin level
+            self.amrBinnings[assignInds] = rebin
+
+    def _copy_final_data(self):
+        """Uses the provided copy function to copy over final, binned data"""
+        # Re-compute the available rebninning levels
         # Copy the rebinned data over to the final output object
         adaptiveMeshedData = copy.deepcopy(self.croppedData)
-        for rebin in forwardBinnings[1:]:
+
+        # Loop through each binning level and copy over the data to the final
+        # output object
+        for rebin in self.rebinLevels[1:]:
             # Up-bin the data to its original shape
-            tmpData = (self.rebinnedData[rebin])[::np.float(1.0/rebin), ::np.float(1.0/rebin)]
+            tmpData = self._AdaptiveMesher__upbinFunction(
+                self.rebinnedData[rebin], rebin
+            )
 
             # Locate the indices assigned to this rebinning level
-            copyInds = np.where(finalBinnings == rebin)
+            copyInds = np.where(self.amrBinnings == rebin)
 
             # Use the supplied dataCopier function to copy data from this
             # rebinning level to the final output data structure
@@ -276,6 +323,7 @@ class AdaptiveMesher(object):
                 copyInds
             )
 
+        # Store the final, rebinned output data
         self.adaptiveMeshedData = adaptiveMeshedData
 
     def run(self, thresholdValue, maxRebins=3):
@@ -284,7 +332,7 @@ class AdaptiveMesher(object):
 
         Parameters
         ----------
-        thresholdValue : `int` or `float`
+        thresholdValue: `int` or `float`
             The minimum value for the binningStatistic (defined by the function)
             provided when initalizing the objet). Any cells which do not meet
             this thresholding value will continue to be rebinned until the
@@ -304,14 +352,25 @@ class AdaptiveMesher(object):
         if not np.float(maxRebins).is_integer():
             raise TypeError('`maxRebins` must be an integer')
 
+        # Compute each of the rebin levels to use and store for later use
+        self.rebinLevels = 2**np.arange(maxRebins + 1)
+
         # Crop data so that rebinning always provides an integer number of cells
-        self._prepare_data(maxRebins)
+        self._crop_data()
 
         # Compute all possible rebinnings which will be later evaluated
-        self._compute_rebinnings(maxRebins)
+        self._compute_rebinnings()
 
         # Parse the rebinned data to decite which binning level is best
-        self._parse_rebinnings(thresholdValue)
+        self._test_rebinnings(thresholdValue)
+
+        # Now that the binning levels have been tested, determine which
+        # binning levels should be used to fill which pixels
+        self._assign_final_binnings()
+
+        # Now that all the optimal binning levels have been computed, copy the
+        # data over from the rebinned data objects into the final output object
+        self._copy_final_data()
 
         # Return the pseudo-AMR data to the user
         return self.adaptiveMeshedData
